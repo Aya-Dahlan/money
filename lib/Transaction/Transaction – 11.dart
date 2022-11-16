1e80263a929cb3b68e6215a2e74a8c865e7677b6
ayaal <ayaalidahlan@gmail.com>
2022-11-16 23:06:28 +0200

    update2

--- lib/Transaction/Transaction – 11.dart
+++ lib/Transaction/Transaction – 11.dart
@@ -10,187 +10,9 @@ class Transaction_11 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffFFFFFF),
-      // body: Stack(
-      //   children: [
-      //     Container(
-      //       width: double.infinity.w,
-      //       height: 400.h,
-      //       color: Color(0xff0E5C46),
-      //     ),
-      //   Column(
-      //         children: [
-      //           Padding(
-      //             padding: EdgeInsets.only(top: 42.h, left: 20.w),
-      //             child: Row(
-      //               children: [
-      //                 IconButton(
-      //                     onPressed: () {},
-      //                     icon: const Icon(
-      //                       Icons.arrow_back_ios,
-      //                       color: Colors.white,
-      //                     )),
-      //                 Text(
-      //                   "Note",
-      //                   style: TextStyle(
-      //                     color: Colors.white,
-      //                     fontSize: 18.sp,
-      //                   ),
-      //                 ),
-      //                 SizedBox(
-      //                   width: 40.w,
-      //                 ),
-      //                 IconButton(
-      //                     onPressed: () {},
-      //                     icon: const Icon(
-      //                       Icons.arrow_back_ios,
-      //                       color: Colors.white,
-      //                     )),
-      //                 Text(
-      //                   "Sep 2022",
-      //                   style: TextStyle(
-      //                     color: Colors.white,
-      //                     fontSize: 18.sp,
-      //                   ),
-      //                 ),
-      //                 IconButton(
-      //                     onPressed: () {},
-      //                     icon: const Icon(
-      //                       Icons.arrow_forward_ios_outlined,
-      //                       color: Colors.white,
-      //                     )),
-      //                 SizedBox(
-      //                   width: 30.w,
-      //                 ),
-      //                 IconButton(
-      //                     onPressed: () {},
-      //                     icon: Icon(
-      //                       Icons.settings_outlined,
-      //                       color: Colors.white,
-      //                     ))
-      //               ],
-      //             ),
-      //           ),
-      //           // SizedBox(
-      //           //   height: 29.h,
-      //           // ),
-      //           Container(
-      //             width: 414.w,
-      //             height: 75.h,
-      //             decoration: BoxDecoration(
-      //               color: Color(0x87317461),
-      //             ),
-      //             child: Padding(
-      //               padding: EdgeInsets.only(top: 22.h, left: 25.w),
-      //               child: Text(
-      //                 "Select items that you want to filter.",
-      //                 style: TextStyle(color: Colors.white),
-      //               ),
-      //             ),
-      //           ),
-      //           Container(
-      //             width: 414.w,
-      //             height: 570.h,
-      //             decoration: BoxDecoration(
-      //               color: Colors.white,
-      //             ),
-      //             child: Column(
-      //               children: [
-      //                 Padding(
-      //                     padding: EdgeInsets.only(
-      //                       top: 5.h,
-      //                     ),
-      //                     child: Column(
-      //                       children: [
-      //                         Container(
-      //                           padding: EdgeInsets.only(left: 20.w),
-      //                           width: 414.w,
-      //                           height: 298.h,
-      //                           color: Color(0xffEBEFF3),
-      //                           child: Column(
-      //                             children: [
-      //                               Row(
-      //                                 children: [
-      //                                   Text(
-      //                                     "Note button setting",
-      //                                     style: TextStyle(
-      //                                         fontWeight: FontWeight.bold),
-      //                                   ),
-      //                                   Spacer(),
-      //                                   IconButton(
-      //                                       onPressed: () {},
-      //                                       icon: Icon(Icons.close)),
-      //                                 ],
-      //                               ),
-      //                               SizedBox(
-      //                                 height: 22.h,
-      //                               ),
-      //                               Center(
-      //                                 child: Container(
-      //                                   width: 349.w,
-      //                                   height: 200.h,
-      //                                   child: Image.asset(
-      //                                     'images/Group 178.png',
-      //                                   ),
-      //                                 ),
-      //                               ),
-      //                             ],
-      //                           ),
-      //                         ),
-      //                         SizedBox(
-      //                           height: 2.h,
-      //                         ),
-      //                         Container(
-      //                           padding:
-      //                               EdgeInsets.only(left: 20.w, right: 32.w),
-      //                           width: 414.w,
-      //                           height: 67.h,
-      //                           color: Color(0xffEBEFF3),
-      //                           child: Row(
-      //                             children: [
-      //                               Text("Show note button on Trans, tab."),
-      //                               Spacer(),
-      //                               Switch(
-      //                                 value: true,
-      //                                 onChanged: (value) {},
-      //                                 activeColor: Color(0xff0E5C46),
-      //                               ),
-      //                             ],
-      //                           ),
-      //                         ),
-      //
-      //
-      //                       ],
-      //                     )),
-      //               ],
-      //             ),
-      //           ),
-      //         ],
-      //       ),
-      //     Positioned(
-      //
-      //
-      //       child: Image.asset('images/Group 408.png'),
-      //       top: 578.h,
-      //       left: 328.w,
-      //       right: 16.w,
-      //
-      //
-      //     ),
-      //
-      //   ],
-      // ),
-
       appBar: AppBar(
-
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.settings_outlined))
-        ],
-        
-//        leading:
-
-  //          IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios),padding: EdgeInsets.zero,),
-
+        backgroundColor: Color(0xff0E5C46),
+        elevation: 0,
         title: Row(
           children: [
             IconButton(
@@ -199,31 +21,163 @@ class Transaction_11 extends StatelessWidget {
                   Icons.arrow_back_ios,
                   color: Colors.white,
                 )),
+            SizedBox(
+              width: 20.w,
+            ),
             Text(
-              'Budget',
+              "Sep 2022",
               style: TextStyle(
-                fontFamily: 'Helvetica Neue',
-                fontSize: 20.sp,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1,
+                color: Colors.white,
+                fontSize: 18.sp,
               ),
             ),
             SizedBox(
-              width: 30.w,
+              width: 20.w,
             ),
-            Text("<   Sep 2022   >",
-            style: TextStyle(
-              fontFamily: 'Helvetica Neue',
-              fontSize: 20.sp,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1,
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.arrow_forward_ios_outlined,
+                  color: Colors.white,
+                )),
+            SizedBox(
+              width: 105.w,
             ),
-              ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ))
           ],
         ),
+      ),
+
+      // SizedBox(
+      //   height: 29.h,
+      // ),
+
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: 414.w,
+                height: 62.h,
+                decoration: BoxDecoration(
+                  color: Color(0xFF0E5C46),
+                ),
+              ),
+              Container(
+                width: 414.w,
+                height: 62.h,
+                decoration: BoxDecoration(
+                  color: Color(0x87317461),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 22.h, left: 25.w),
+                  child: Text(
+                    "Select items that you want to filter.",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+          Container(
+            width: 414.w,
+            height: 570.h,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Column(
+              children: [
+
+                Padding(
+                    padding: EdgeInsets.only(
+                      top: 5.h,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(left: 20.w),
+                          width: 414.w,
+                          height: 298.h,
+                          color: Color(0xffEBEFF3),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    "Note button setting",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Spacer(),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.close)),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 22.h,
+                              ),
+                              Center(
+                                child: Container(
+                                  width: 349.w,
+                                  height: 200.h,
+                                  child: Image.asset(
+                                    'images/Group 178.png',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 2.h,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 20.w, right: 32.w),
+                          width: 414.w,
+                          height: 67.h,
+                          color: Color(0xffEBEFF3),
+                          child: Row(
+                            children: [
+                              Text("Show note button on Trans, tab."),
+                              Spacer(),
+                              Switch(
+                                value: true,
+                                onChanged: (value) {},
+                                activeColor: Color(0xff0E5C46),
+                              ),
+                            ],
+                          ),
+                        ),
+                           SizedBox(
+                             height: 110.h,
+                           ),
+
+                           Padding(
+                             padding:  EdgeInsets.only(right:20.w ),
+                             child: Row(
+                               mainAxisAlignment: MainAxisAlignment.end,
+                               children: [
+                                 Image.asset('images/Group 408.png'),
+                               ],
+                             ),
+                           ),
 
 
+                      ],
+                    )),
 
+              ],
+            ),
+          ),
+
+        ],
       ),
     );
   }
